9a45601ffb1483682de8659b894763402e604e93
Aclaputra <muhamadacla22@gmail.com>
2022-06-29 08:58:28 +0700

    feat: modified routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EmployeesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/get/employee/list', 
+    [EmployeesController::class, 'getEmployeeList']
+)->name('employee.list');
