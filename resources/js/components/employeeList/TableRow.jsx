88163dead99b1cc042c8cd61c500a5dc52b17f9c
Aclaputra <muhamadacla22@gmail.com>
2022-06-29 09:00:36 +0700

    feat: added tablerow.jsx

---
+++ resources/js/components/employeeList/TableRow.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class TableRow extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <tr>
+                <th scope="row">{this.props.data.id}</th>
+                <td>{this.props.data.employee_name}</td>
+                <td>{this.props.data.salary}</td>
+                <td>@mdo</td>
+            </tr>
+        )
+    }
+}
