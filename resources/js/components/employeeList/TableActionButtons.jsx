dfbd01e2ff846326394e776edd8bb3104919d9f1
Aclaputra <muhamadacla22@gmail.com>
2022-06-30 17:48:39 +0700

    feat: modified tableactionbutton

--- resources/js/components/employeeList/TableActionButtons.jsx
+++ resources/js/components/employeeList/TableActionButtons.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ViewModal from '../modals/ViewModal'
+import UpdateModal from '../modals/UpdateModal';
 
 export default class TableActionButtons extends Component {
 
@@ -44,10 +45,12 @@ export default class TableActionButtons extends Component {
           className="btn btn-info" 
           type="button"
           data-bs-toggle="modal" 
-          data-bs-target="#updateModal"
+          data-bs-target={"#updateModal" + this.props.eachRowId}
+          onClick={ () => {this.getEmployeeDetails(this.props.eachRowId)} }
         >
           Update
         </button>
+        <UpdateModal modalId={ this.props.eachRowId } employeeData={ this.state }/>
 
         <button 
           className="btn btn-danger" 
@@ -57,7 +60,7 @@ export default class TableActionButtons extends Component {
         >
           Delete
         </button>
-
+        {/* <DeleteModal modalId={ this.props.eachRowId } employeeData={ this.state }/> */}
       </div>
     )
   }
