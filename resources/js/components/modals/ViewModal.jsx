a4cfeaf028e5c0f1320aeaf3cc6ac81d405899b1
Aclaputra <muhamadacla22@gmail.com>
2022-06-30 17:48:56 +0700

    feat: modified viewModal

--- resources/js/components/modals/ViewModal.jsx
+++ resources/js/components/modals/ViewModal.jsx
@@ -12,7 +12,7 @@ export default class ViewModal extends Component {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Employee Details</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
@@ -22,7 +22,6 @@ export default class ViewModal extends Component {
                             </div>
                             <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
